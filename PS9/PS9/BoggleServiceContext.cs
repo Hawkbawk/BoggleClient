8b54ed385abf88c50ea8d7b92554c5fb14f2052e
CURTIS LIN <u1116418@umail.utah.edu>
2019-03-28 18:25:49 -0600

    Deleted Spreadsheet from PS9

--- PS9/PS9/BoggleServiceContext.cs
+++ PS9/PS9/BoggleServiceContext.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ISpreadsheetView
+namespace PS9
 {
-    public class Class1
+    class BoggleServiceContext
     {
     }
 }
